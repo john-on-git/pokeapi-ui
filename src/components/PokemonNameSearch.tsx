a2a1d3c6fb7cbc854500fa38cb4b46fa6b6a099f
John <johngreggwebster@gmail.com>
2025-10-19 19:38:35 +0100

    static errors

--- src/components/PokemonNameSearch.tsx
+++ src/components/PokemonNameSearch.tsx
@@ -1,24 +1,24 @@
-import { ReactNode, useRef, useState } from "react";
+import { ReactNode, useState } from "react";
 import "./PokemonNameSearch.css";
 
 interface Props {
     pokemonName: string;
-    setPokemonNameCallbackFn: ((pokemonName:string) => void);
+    setPokemonNameCallbackFn: ((pokemonName: string) => void);
     allAutocompletions: Map<string, Set<string>>;
 }
-export default function PokemonNameSearch({pokemonName, setPokemonNameCallbackFn, allAutocompletions}: Props) {
+export default function PokemonNameSearch({ pokemonName, setPokemonNameCallbackFn, allAutocompletions }: Props) {
     const [isFocused, setIsFocused] = useState(false);
 
     const asList: ReactNode[] = [];
-    
+
     //get the autocompletions for the current pokemonName
     const autoCompletions = allAutocompletions.get(pokemonName);
-    if(autoCompletions!==undefined) {
+    if (autoCompletions !== undefined) {
         autoCompletions.forEach((autocompletion) => asList.push(
-            <button 
-                className="autocompletion poke-button" 
-                type="button" 
-                key={autocompletion} 
+            <button
+                className="autocompletion poke-button"
+                type="button"
+                key={autocompletion}
                 onClick={() => {
 
                     setPokemonNameCallbackFn(autocompletion)
@@ -37,21 +37,21 @@ export default function PokemonNameSearch({pokemonName, setPokemonNameCallbackFn
                     <input
                         className="name-search bold"
                         type="text"
-                        value = {pokemonName}
+                        value={pokemonName}
                         onChange={(e) => {
                             setPokemonNameCallbackFn(e.target.value);
                         }}
-                        onFocus={()=>setIsFocused(true)}
-                        onBlur={()=>setTimeout(()=>setIsFocused(false), 100)}
+                        onFocus={() => setIsFocused(true)}
+                        onBlur={() => setTimeout(() => setIsFocused(false), 100)}
                     >
                     </input>
                     {
                         isFocused ?
-                        <div className="autocompletions" style={{display:((asList.length===0) ? "none" : "")}}>
-                            {asList}
-                        </div>
-                        :
-                        null
+                            <div className="autocompletions" style={{ display: ((asList.length === 0) ? "none" : "") }}>
+                                {asList}
+                            </div>
+                            :
+                            null
                     }
                 </div>
             </div>
--- src/components/PokemonViewer.tsx
+++ src/components/PokemonViewer.tsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from 'react';
 import MoveList from '../components/MoveList';
-import { Pokemon } from '../interfaces/Pokemon';
-import { SpriteDisplay } from './SpriteDisplay';
-import './PokemonViewer.css';
-import PokemonNameSearch from './PokemonNameSearch';
-import BaseStatsDisplay from './BaseStatsDisplay';
 import { PokeAPIPokemonSpriteURL } from '../interfaces/PokeAPIURLs';
+import { Pokemon } from '../interfaces/Pokemon';
+import BaseStatsDisplay from './BaseStatsDisplay';
 import FormSelector from './FormSelector';
+import PokemonNameSearch from './PokemonNameSearch';
+import './PokemonViewer.css';
+import { SpriteDisplay } from './SpriteDisplay';
 
 export default function PokemonViewer() {
-    
+
     /*
         <CryPlayer/>
         <MoveList/>
         <StatList/>
     */
 
-    const [searchAutocomplete, setSearchAutocomplete] = useState<Map<string,Set<string>>>(new Map<string,Set<string>>()); //contains substrings of all pokemon names, and the corresponding autocomplete possibilites. Map is faster than Object for n≈1000 according to the data in this article https://www.zhenghao.io/posts/object-vs-map
+    const [searchAutocomplete] = useState<Map<string, Set<string>>>(new Map<string, Set<string>>()); //contains substrings of all pokemon names, and the corresponding autocomplete possibilites. Map is faster than Object for n≈1000 according to the data in this article https://www.zhenghao.io/posts/object-vs-map
     const [pokemonName, setPokemonName] = useState<string>("pikachu"); //use mew for movelist testing because it can learn moves of every type
     const [pokemon, setPokemon] = useState<Pokemon | null>(null);
     const [sprite, setSprite] = useState<PokeAPIPokemonSpriteURL | null>(null);
@@ -25,21 +25,24 @@ export default function PokemonViewer() {
         async function BuildAutocomplete() {
             await fetch(`https://pokeapi.co/api/v2/pokemon?limit=5000`).then(
                 async (res) => {
-                    if(res.ok) {
-                        const allPokemon: {results: {name: string}[]} = await res.json();
+                    if (res.ok) {
+                        const allPokemon: { results: { name: string }[] } = await res.json();
                         //iterate every substring of every pokemon name, and build up a dictionary of autocompletions
                         //O(n)
                         const subStrings: string[] = [];
-                        for(const {name} of allPokemon.results) {
-                            const nMinOne = name.length-1;
-                            for(let i=1; i<nMinOne; i++) {
-                                subStrings.push(name.substring(0,i));
-                                
+                        for (const { name } of allPokemon.results) {
+                            const nMinOne = name.length - 1;
+                            for (let i = 1; i < nMinOne; i++) {
+                                subStrings.push(name.substring(0, i));
+
                                 //initialize the list if one does not already exists
-                                if(searchAutocomplete.get(subStrings[subStrings.length-1])===undefined) {
-                                    searchAutocomplete.set(subStrings[subStrings.length-1], new Set<string>());
+                                if (searchAutocomplete.get(subStrings[subStrings.length - 1]) === undefined) {
+                                    searchAutocomplete.set(subStrings[subStrings.length - 1], new Set<string>());
                                 }
-                                searchAutocomplete.get(subStrings[subStrings.length-1])!.add(name); //bang operator suppresses a typescript warning here (possible === undefined), pretty sure this is a static analysis bug but might not be  
+                                const suggestions = searchAutocomplete.get(subStrings[subStrings.length - 1]);
+                                if (suggestions !== undefined) {
+                                    suggestions.add(name)
+                                }
                             }
                             subStrings.length = 0; //clear the array
                         }
@@ -51,16 +54,16 @@ export default function PokemonViewer() {
     }, []);
 
     useEffect(() => {
-        async function FetchPokemonByName(pokemonName:(string)) {
-            if(pokemonName==="") { //a blank string result in a call to the index endpoint, returns OK but with a different type
+        async function FetchPokemonByName(pokemonName: (string)) {
+            if (pokemonName === "") { //a blank string result in a call to the index endpoint, returns OK but with a different type
                 return null;
             }
             else {
                 await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
                     async (res) => {
-                        if(res.ok) {
-                            const newPokemon = await res.json();                 
-                            if(newPokemon!=null) {
+                        if (res.ok) {
+                            const newPokemon = await res.json();
+                            if (newPokemon != null) {
                                 setPokemon(newPokemon);
                                 setSprite(newPokemon.sprites.front_default); //also set the initial sprite to the default form
                             }
@@ -77,7 +80,7 @@ export default function PokemonViewer() {
 
     return (
         <div className="pokemon-viewer">
-            <MoveList moveProviders={pokemon===null ? [] : pokemon.moves}/>
+            <MoveList moveProviders={pokemon === null ? [] : pokemon.moves} />
             <div className="pokemon-viewer-mid-col">
                 <PokemonNameSearch
                     pokemonName={pokemonName}
@@ -88,24 +91,24 @@ export default function PokemonViewer() {
                     imageURL={sprite}
                     onSpriteClick={(event) => {
                         const mainCssClass = "sprite-bounce";
-                        if(pokemon!==null && !event.currentTarget.classList.contains(mainCssClass)) {
-                            const cssClasses = [mainCssClass, (((Math.round(Math.random()*2)%2)===0) ? "sprite-twist-left" : "sprite-twist-right")];
+                        if (pokemon !== null && !event.currentTarget.classList.contains(mainCssClass)) {
+                            const cssClasses = [mainCssClass, (((Math.round(Math.random() * 2) % 2) === 0) ? "sprite-twist-left" : "sprite-twist-right")];
                             //play the cry audio
                             const cry = new Audio(pokemon.cries.latest);
-                            cry.addEventListener("ended", (function (this: Element) { this.classList.remove(... cssClasses); } ).bind(event.currentTarget)); //listener to remove the visual effect class when the cry is finsihed
+                            cry.addEventListener("ended", (function (this: Element) { this.classList.remove(...cssClasses); }).bind(event.currentTarget)); //listener to remove the visual effect class when the cry is finsihed
                             cry.play();
 
                             //do visual effect
-                            event.currentTarget.classList.add(... cssClasses);
+                            event.currentTarget.classList.add(...cssClasses);
                         }
                     }}
                 />
                 <FormSelector
-                    forms={pokemon===null ? [] : pokemon.forms /*extract just the url from the wrapper object*/ }
+                    forms={pokemon === null ? [] : pokemon.forms /*extract just the url from the wrapper object*/}
                     setSpriteCallbackFn={setSprite}
                 />
             </div>
-            <BaseStatsDisplay stats={pokemon===null ? null : pokemon.stats}/>
+            <BaseStatsDisplay stats={pokemon === null ? null : pokemon.stats} />
         </div>
     );
 }
